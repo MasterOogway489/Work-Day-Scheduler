3d3b6b84ffcac980901c0288ae6a05a37b07acfe
Sam Wilson <sammy011116@gmail.com>
2023-07-18 19:15:53 -0600

    Added color to scheduler

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,8 +2,33 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
   var currentDay = dayjs();
+  // var currentHour = currentDay.hour();
+  var currentHour = 11;
+
 $("#currentDay").text(currentDay.format("MMMM D, YYYY"));  
+
+console.log(currentHour);
+
+$(".time-block").each(function() {
+
+  var hour = parseInt($(this).attr("id"));
+
+  if (hour < currentHour) {
+    $(this).addClass("past");
+  } 
+  else if (hour === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+  }
+  else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+  }
+});
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,6 +46,6 @@ $("#currentDay").text(currentDay.format("MMMM D, YYYY"));
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // TODO: Add code to display the current date in the header of the page. v
 });
 
